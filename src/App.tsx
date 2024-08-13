e4900f326d58910044ccaab7f59c98742bd8c5ae
Faheem <mrfaheemuddin7@gmail.com>
2024-08-13 16:47:56 +0500

    Work

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,9 @@ import ParticlesComponent from "./Components/Particles";
 import "./App.css";
 import "./style.css";
 import "./index.css";
-import { lazy, Suspense, useEffect, useState } from "react";
+import { lazy, Suspense } from "react";
 import Loader from "./Components/Loader";
 function App() {
-  // const [load, setLoad] = useState(true);
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setLoad(false);
-  //   }, 1200);
-
-  //   return () => clearTimeout(timer);
-  // }, []);
   const Home = lazy(() => import("./pages/Home"));
   return (
     <>
